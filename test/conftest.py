5441df39d717cbc305500ec444ca0ac3c6721d11
Oskar Hovmoller Dinesen <ohodi@dtu.dk>
2026-01-06 11:44:21 +0100

    Updated testsuite

--- test/conftest.py
+++ test/conftest.py
@@ -1,6 +1,39 @@
 import pytest
 from pathlib import Path
 from loguru import logger
+import shutil
+
+from simbench.analysis import Analysis, get_all_classifiers, get_all_tools
+from simbench.build import Suite, Builder
+
+
+@pytest.fixture(scope="session")
+def test_bld():
+    return Builder(logger)
+
+
+@pytest.fixture(scope="session")
+def test_suite():
+    return Suite(Path("./test/testfiles/"))
+
+
+@pytest.fixture(params=[tool for tool in get_all_tools()])
+def test_tool(request):
+    return request.param
+
+
+@pytest.fixture(scope="session")
+def test_classifiers():
+    return get_all_classifiers()
+
+
+@pytest.fixture(scope="function")
+def test_analysis(test_tool, test_suite, test_classifiers):
+    analysis = Analysis(tool=test_tool, suite=test_suite, classifiers=test_classifiers)
+    yield analysis
+
+    # cleanup
+    shutil.rmtree(analysis.default_path)
 
 
 #
--- test/test_build.py
+++ test/test_build.py
@@ -1,3 +1,49 @@
-def test_comp_node(test_analysis):
-    print(test_analysis.comp_node)
-    assert False
+from simbench.build import Node, schema
+from simbench.tables import (
+    CompressionTable,
+    DistanceTable,
+    PairwiseCompressionTable,
+    ClassificationTable,
+)
+import polars as pl
+
+
+def test_comp_node(test_analysis, test_bld):
+    comp_node = test_analysis.comp_node
+    assert isinstance(comp_node, Node)
+
+    comp_df = comp_node.pull(test_bld)
+    assert isinstance(comp_df, pl.LazyFrame)
+
+    assert schema(CompressionTable) == comp_df.collect().schema
+
+
+def test_pair_node(test_analysis, test_bld):
+    pair_node = test_analysis.pair_node
+    assert isinstance(pair_node, Node)
+
+    pair_df = pair_node.pull(test_bld)
+    assert isinstance(pair_df, pl.LazyFrame)
+
+    assert schema(PairwiseCompressionTable) == pair_df.collect().schema
+
+
+def test_dist_node(test_analysis, test_bld):
+    dist_node = test_analysis.distance_node
+    assert isinstance(dist_node, Node)
+
+    dist_df = dist_node.pull(test_bld)
+    assert isinstance(dist_df, pl.LazyFrame)
+    assert schema(DistanceTable) == dist_df.collect().schema
+
+
+def test_classification_node(test_analysis, test_bld):
+    cl_nodes = test_analysis.classification_nodes
+    assert isinstance(cl_nodes, dict)
+
+    cl_df_list = [cl_df.pull(test_bld) for _, cl_df in cl_nodes.items()]
+    assert all([isinstance(cl_df, pl.LazyFrame) for cl_df in cl_df_list])
+
+    assert all(
+        [schema(ClassificationTable) == cl_df.collect().schema for cl_df in cl_df_list]
+    )
